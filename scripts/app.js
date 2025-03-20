2e0f9dc95e1f893888598ef3c81d9ace93666d95
Jorge Luis Huerta Vargas <e-jlhuerta@neoris.com>
2025-03-20 12:35:25 -0600

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+document.getElementById("loginForm")?.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "1234") {
+        alert("Inicio de sesión exitoso");
+        window.location.href = "index.html";
+    } else {
+        alert("Usuario o contraseña incorrectos");
+    }
+});
